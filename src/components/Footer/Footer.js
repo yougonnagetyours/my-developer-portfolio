199b62047e8b0cd5eec371d263710f87b6a55fc3
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-11-27 11:16:23 +0100

    almost end

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,24 @@ const Footer = () => {
           <LinkTitle>Call me</LinkTitle>
           <LinkItem href="tel:694-773-906">694 773 906</LinkItem>
         </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>E-mail</LinkTitle>
+          <LinkItem href="mailto:michal.potoczny.92@gmail.com">michal.potoczny.92@gmail.com</LinkItem>
+        </LinkColumn>
       </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Make the future more simple</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/            yougonnagetyours">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
     </FooterWrapper>
   );
 };
