0b716ebb3b57fcf91071032437583e94902b9644
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-11-27 11:01:09 +0100

    styling footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,14 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call me</LinkTitle>
+          <LinkItem href="tel:694-773-906">694 773 906</LinkItem>
+        </LinkColumn>
+      </LinkList>
+    </FooterWrapper>
   );
 };
 
