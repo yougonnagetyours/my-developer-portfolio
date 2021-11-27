8acddd104d2ffa6087fecd0c86a585cb5e979874
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-11-27 10:11:37 +0100

    Technologies section done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,40 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListTitle>Front-End</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          ReacjJS
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListTitle>Back-End</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          Node.js
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListTitle>UI/UX</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          tools like figma
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
