a474aeb1678e6299eaec25c7e96739c3c1194543
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-11-29 08:26:03 +0000

    change bcg color

--- src/themes/default.js
+++ src/themes/default.js
@@ -6,7 +6,7 @@ export default {
   },
   // Colors for layout
   colors: {
-    primary1: "hsl(204,23.8%,95.9%)",
+    primary1: "#243D70",
     background1: "#0F1624",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
