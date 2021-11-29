cbc60fbec5f6d43d51d8f7b26e5b3564aa2a78eb
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-11-29 12:09:13 +0000

    still style

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,7 +5,7 @@ export const projects = [
                   CSS. The shop is joined to Commerce.js via API to add products to the shop, manage cart, control orders 
                   etc. For this shop is also implemented Stripe for card transactions.`,
       image: '/images/1.png',
-      tags: ['React', 'JavaScript', 'Tailwind', 'Mobile first', 'API', 'Stripe', 'Commerce.js',],
+      tags: ['React', 'Tailwind', 'Stripe',],
     source: 'https://github.com/yougonnagetyours/kangur-makes-candles',
     visit: 'https://kangur-makes-candles.netlify.app/',
     id: 0,
@@ -16,7 +16,7 @@ export const projects = [
                  create header menu, menu button etc. This site is Single Page Application. Controlled by Git with remote 
                  repository on GitHub.`,
     image: '/images/2.png',
-    tags: ['React', 'JavaScript', 'Tailwind', 'Mobile first', 'Git', 'GitHub'],
+    tags: ['React', 'JavaScript', 'Tailwind',],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 1,
