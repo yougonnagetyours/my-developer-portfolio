ca028c0e1792eddec1164d2ba474834efcb9344c
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-11-29 11:50:06 +0000

    style projects

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -27,7 +27,7 @@ export const BlogCard = styled.div`
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
-  height: 25vh;
+  height: 50vh;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
