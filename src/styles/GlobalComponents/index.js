8d4c018ac6c60c725ea44a18933c3dfc4735737a
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-11-30 13:50:35 +0000

    change section divider's gradient

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -83,8 +83,8 @@ export const SectionDivider = styled.div`
   border-radius: 10px;
   background-color: #fff;
   background: ${(props) => props.colorAlt ? 
-    'linear-gradient(270deg, #F46737 0%, #945DD6 100%)' :
-    'linear-gradient(270deg, #13ADC7 0%, #945DD6 100%)'};
+    'linear-gradient(270deg, #16A085 0%, #F4D03F 100%)' :
+    'linear-gradient(270deg, #16A085 0%, #F4D03F 100%)'};
 
     margin: ${(props) => props.divider ? "4rem 0" : "" };
 
