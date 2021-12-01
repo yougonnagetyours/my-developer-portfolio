769fd9869d410feffd9427c4c64bcc9cd2b5d334
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-01 14:03:27 +0000

    Add pic to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { Img } from '../Projects/ProjectsStyles';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
+
+import heroPic from '/images/hero-1.jpg'
 
 const Hero = () => (
   <Section row nopadding>
@@ -15,6 +18,7 @@ const Hero = () => (
         There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
       </SectionText>
       <Button onClick={() => window.location = "https://google.com"}>Learn more</Button>
+      <Img source={heroPic} />
     </LeftSection>
   </Section>
 );
