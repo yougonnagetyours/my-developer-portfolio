7f2b5bb0dbb94f88186f246a2abddcfe56c1cca1
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-01 14:37:36 +0000

    one more

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -18,7 +18,7 @@ const Hero = () => (
         There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
       </SectionText>
       <Button onClick={() => window.location = "https://google.com"}>Learn more</Button>
-      <img src={heroImage.image} />
+      <Img source={heroImage.image} />
     </LeftSection>
   </Section>
 );
