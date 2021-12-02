30337e3754e272dc3c9c5dc9ada9405e53746738
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-02 12:55:45 +0000

    change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <>
       <Head>
-          <title>My page title</title>
+          <title>Michał Potoczny | Portfolio</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
       <Layout>
