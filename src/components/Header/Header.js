60bd7b0dbb1a5f28ae396cde0058dacc0a2e7651
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-02 14:32:26 +0000

    Change icon next to portfolio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { FiActivity } from "react-icons/fi";
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
@@ -9,7 +9,7 @@ const Header = () =>  (
   <Container>
     <Div1>
       <a style={{display: "flex", alignItems: "center", color: "white", marginBottom: "20px"}}>
-        <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        <FiActivity size="3rem" /> <Span>Portfolio</Span>
       </a>
     </Div1>
     <Div2>
