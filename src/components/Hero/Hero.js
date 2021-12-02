733080c914c9b3c1c3635f104a11ba7ed8a76a55
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-02 18:25:22 +0100

    change hero for better flex

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,11 +15,13 @@ const Hero = () => (
         I am Michał, <br />
         React Developer
       </SectionTitle>
-      <div style={{display: 'flex', flexWrap: 'wrap'}}>
-        <SectionText>
-          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-        </SectionText>
-        <div style={{marginBottom: '20px', width: '200%'}}>
+      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+        <div style={{marginBottom: '20px', width: '60%'}}>
+          <SectionText>
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+          </SectionText>
+        </div>
+        <div style={{marginBottom: '20px', width: '35%', minWidth: '120px'}}>
           <Img src={images.heroImage} />
         </div>
       </div>  
