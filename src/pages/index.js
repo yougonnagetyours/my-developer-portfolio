02b5d2be85da513dbdf48a89b0957e3f535e62a4
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-02 12:53:41 +0000

    Add page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,24 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Head from 'next/head'
 
 const Home = () => {
   return (
-    <Layout>
-      <Section>
-        <Hero />
-      </Section>
-      <Technologies />
-      <Projects />      
-      <Timeline />
-    </Layout>
+    <>
+      <Head>
+          <title>My page title</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+      <Layout>
+        <Section>
+          <Hero />
+        </Section>
+        <Technologies />
+        <Projects />      
+        <Timeline />
+      </Layout>
+    </>
   );
 };
 
