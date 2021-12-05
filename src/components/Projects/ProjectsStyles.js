e0dc40f0306fc9d95812585d83d9384b25180285
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-05 17:05:07 +0100

    Update ps

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -44,7 +44,7 @@ export const TitleContent = styled.div`
 export const HeaderThree = styled.h3`
   font-weight: 500;
   letter-spacing: 2px;
-  color: #F4D03F;
+  color: #16A085;
   padding: .5rem 0;
   font-size: ${(props) => props.title ? '3rem' : '2rem'};
 `;
