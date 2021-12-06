3608610a3db4e7d7943c2bbbc71a790bfb4ba80f
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-06 21:25:40 +0100

    add tailwind icon

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { DiReact } from 'react-icons/di';
 import { FaGitAlt } from 'react-icons/fa';
 import { AiFillGithub } from 'react-icons/ai';
+import { SiTailwindcss } from 'react-icons/si';
 import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListTitle } from './TechnologiesStyles';
@@ -50,7 +51,7 @@ const Technologies = () =>  (
       </ListItem>
       <ListItem>
         <picture style={{color: "#be9f00"}}>
-          <IoLogoJavascript size="3rem" />
+          <SiTailwindcss size="3rem" />
         </picture>
         <ListContainer>
           <ListTitle>Tailwind CSS</ListTitle>
