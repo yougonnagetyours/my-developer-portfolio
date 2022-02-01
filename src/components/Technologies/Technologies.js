810f0593a9aabcd8711dc1e5e558868e27c6ffda
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-02-01 12:48:24 +0100

    Change style of technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { DiReact, DiSass } from 'react-icons/di';
+import { DiReact, DiSass, DiResponsive } from 'react-icons/di';
 import { FaGitAlt } from 'react-icons/fa';
-import { AiFillGithub } from 'react-icons/ai';
+import { AiFillApi } from 'react-icons/ai';
 import { SiTailwindcss, SiGatsby, SiWordpress } from 'react-icons/si';
 import { BsFillBootstrapFill } from 'react-icons/bs';
 import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListTitle } from './TechnologiesStyles';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <Section id="tech">
@@ -14,8 +14,7 @@ const Technologies = () =>  (
     <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      Main technology I use is React, but in purpose to e.g styling etc. I use CSS, but more often there is Tailwind CSS, 
-      Styled Components and sometimes - Material UI. Very often when I use API's or CMS's I used to use official documentacion which is almost always compatible with REST API approach or I make schemas and queries in GraphQL query language.
+      Main technology I use is React and JavaScript. In purpose to styling I used to use CSS, but more often there is one of frameworks: Tailwind CSS, Styled Components and Material UI. Very often when I use API or CMS I used to use official documentacion which is almost always compatible with REST API approach or I make schemas and queries in GraphQL query language.
     </SectionText>
     <List>
       <ListItem>
@@ -24,6 +23,9 @@ const Technologies = () =>  (
         </picture>
         <ListContainer>
           <ListTitle>React</ListTitle>
+          <ListParagraph>Next.js</ListParagraph>
+          <ListParagraph>Create React App</ListParagraph>
+          <ListParagraph>Gatsby</ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
@@ -32,6 +34,7 @@ const Technologies = () =>  (
         </picture>
         <ListContainer>
           <ListTitle>JavaScript</ListTitle>
+          <ListParagraph>Object Oriented Programming</ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
@@ -40,14 +43,8 @@ const Technologies = () =>  (
         </picture>
         <ListContainer>
           <ListTitle>Git</ListTitle>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture style={{color: "#be9f00"}}>
-          <AiFillGithub size="2rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>GitHub</ListTitle>
+          <ListParagraph>GitHub</ListParagraph>
+          <ListParagraph>Sourcetree</ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
@@ -55,47 +52,22 @@ const Technologies = () =>  (
           <SiTailwindcss size="2rem" />
         </picture>
         <ListContainer>
-          <ListTitle>Tailwind CSS</ListTitle>
+          <ListTitle>Styling</ListTitle>
+          <ListParagraph>Responsive Web Design</ListParagraph>
+          <ListParagraph>Tailwind CSS</ListParagraph>
+          <ListParagraph>Styled Components</ListParagraph>
+          <ListParagraph>Material UI</ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture style={{color: "#be9f00"}}>
-          <BsFillBootstrapFill size="2rem" />
+          <AiFillApi size="2rem" />
         </picture>
         <ListContainer>
-          <ListTitle>Bootstrap</ListTitle>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture style={{color: "#be9f00"}}>
-          <DiSass size="2rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>SASS</ListTitle>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture style={{color: "#be9f00"}}>
-          <SiGatsby size="2rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>Gatsby</ListTitle>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture style={{color: "#be9f00"}}>
-          <SiWordpress size="2rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>Wordpress</ListTitle>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture style={{color: "#be9f00"}}>
-          <IoLogoHtml5 size="2rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>HTML</ListTitle>
+          <ListTitle>API</ListTitle>
+          <ListParagraph>REST API</ListParagraph>
+          <ListParagraph>GraphQL</ListParagraph>
+          <ListParagraph>CMS</ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
@@ -104,6 +76,15 @@ const Technologies = () =>  (
         </picture>
         <ListContainer>
           <ListTitle>CSS</ListTitle>
+          <ListParagraph>SASS</ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture style={{color: "#be9f00"}}>
+          <IoLogoHtml5 size="2rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>HTML</ListTitle>
         </ListContainer>
       </ListItem>
     </List>
