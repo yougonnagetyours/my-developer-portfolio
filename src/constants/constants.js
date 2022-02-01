64c101b1fc460e38db5364184d96f07db8485332
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-02-01 14:22:25 +0100

    Change projects order

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,14 @@
 export const projects = [
   {
+    title: 'Travelfolio',
+    description:`Travelfolio is the blog created with Next.js and it is connected with headless CMS - GraphCMS. All queries are made via GraphQL. People have opporunity to comment each post and they could save their nick and email in the browser local storage.`,
+    image: '/images/travelfolio.png',
+    tags: ['Next.js', 'GraphQL', 'CMS',],
+    source: 'https://github.com/yougonnagetyours/travelfolio',
+    visit: 'https://travelfolio.vercel.app',
+    id: 1,
+  },
+  {
     title: 'E-commerce shop',
     description: `This project is made with Create React App. Main technology is React.js, styling via Tailwind 
                   CSS. The shop is joined to Commerce.js via API to add products to the shop, manage cart, control orders 
@@ -9,15 +18,6 @@ export const projects = [
     source: 'https://github.com/yougonnagetyours/kangur-makes-candles',
     visit: 'https://kangur-makes-candles.netlify.app/',
     id: 0,
-  },
-  {
-    title: 'Travelfolio',
-    description:`Travelfolio is the blog created with Next.js and it is connected with headless CMS - GraphCMS. All queries are made via GraphQL. People have opporunity to comment each post and they could save their nick and email in the browser local storage.`,
-    image: '/images/travelfolio.png',
-    tags: ['Next.js', 'GraphQL', 'CMS',],
-    source: 'https://github.com/yougonnagetyours/travelfolio',
-    visit: 'https://travelfolio.vercel.app',
-    id: 1,
   },
   {
     title: 'Photo Portfolio',
