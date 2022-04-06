1d80535dc766ba58c12ea1f30601e2d10651a62b
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-04-06 14:08:05 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,9 @@
 export const projects = [
   {
     title: 'Travelfolio',
-    description:`Travelfolio is the blog created with Next.js – React app skeleton, and it is connected to content management system - GraphCMS. All queries are made via GraphQL. Users have possibility to comment each post and they could save their nicks and emails in the browser local storage. Styling is also made with Tailwind.`,
+    description:`Travelfolio is the blog created with Next.js and it is connected to content management system - GraphCMS. 
+    All queries are made via GraphQL. Users have possibility to comment each post and they could save their nicks and emails in the browser local storage. 
+    Styling is made with Tailwind.`,
     image: '/images/travelfolio.png',
     tags: ['Next.js', 'GraphQL', 'CMS',],
     source: 'https://github.com/yougonnagetyours/travelfolio',
@@ -10,7 +12,11 @@ export const projects = [
   },
   {
     title: 'E-commerce shop',
-    description: `Kangur Makes Candles is an e-commerce shop which is made using React.js. All the asynchronous queries are handled via state management library - Redux Styling is made with Tailwind – the CSS framework. The shop is connected to Commerce.js system which allows shop owner to add products to the shop, manage cart, control orders, bills, invoices etc. For this shop I also implemented Stripe – payment processor for card transactions.`,
+    description: `Kangur Makes Candles is an e-commerce shop which is made using React.js. 
+    All the asynchronous queries are handled via state management library - Redux. 
+    Styling is made with Tailwind – the CSS framework. The shop is connected to Commerce.js 
+    system which allows shop owner to add products to the shop, manage cart, control orders, bills, invoices etc. 
+    For this shop I also implemented Stripe – payment processor for card transactions.`,
       image: '/images/1.png',
       tags: ['React', 'Tailwind', 'Stripe',],
     source: 'https://github.com/yougonnagetyours/kangur-makes-candles',
